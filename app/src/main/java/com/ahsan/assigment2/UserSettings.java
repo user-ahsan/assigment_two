b8e2172d49177e0f167ea90d4dcc3829a27568ec
user-ahsan <ahsansheikhoffice@gmail.com>
2024-12-05 10:01:41 +0500

    main acitvity divided into two aprts and fragment files added

---
+++ app/src/main/java/com/ahsan/assigment2/UserSettings.java
@@ -0,0 +1,21 @@
+package com.ahsan.assigment2;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+class UserSettings extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_user_settings, container, false);
+    }
+}
